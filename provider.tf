9955f359829a586ad1a8b24a8decc4d5599be641
mohithuria2608 <68209936+mohithuria2608@users.noreply.github.com>
2021-09-07 23:14:57 +0530

    Add files via upload

---
+++ provider.tf
@@ -0,0 +1,22 @@
+/*provider "aws" {
+  #region = var.region
+  region     = "ap-southeast-1"
+ 
+
+  tags_as_map = {
+
+    Environment = "dev"
+  }
+
+  user_data = <<-EOT
+  #!/bin/bash
+  echo "Hello Terraform!"
+  EOT
+  
+}*/
+
+
+/*provider "aws" {
+  region     = "ap-southeast-1"
+}
+*/
